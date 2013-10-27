83b3dbb58840fcd159e2ee6503ae55d666c19621
Djordje Mijatovic <djjolem@gmail.com>
2013-10-27 15:39:00 +0100

    redesign

--- index.php
+++ index.php
@@ -1,34 +1,42 @@
 <html>
 <head>
+    <title> Link to ... </title>
+    <meta name="description" content="Link shortener"/>
+    <meta http-equiv="content-type" content="text/html;charset=utf-8" />
+
+    <link rel="stylesheet" href="style.css" type="text/css"/>
 </head>
 
 <body>
-    <h5>Create short link</h5>
+<?php
+    include 'DBConnection.php';
 
-    <?php
-        include 'DBConnection.php';
+    $hash = $_GET["hash"];
+    if (isset($hash)){
+        $model = new DBConnection;
+        $link = $model->getLink($hash);
+        echo 'Go to link: ' . $link;
 
-
-        $hash = $_GET["hash"];
-        if (isset($hash)){
-            $model = new DBConnection;
-            $link = $model->getLink($hash);
-            echo 'Go to link: ' . $link;
-
-
-            if ($link != ""){
-                header("Location: " . 'http://'. $link);
-            } else {
-                echo '<div> Cant find link with hash: ' . $hash . ' </div>';
-            }
+        if ($link != ""){
+            header("Location: " . 'http://'. $link);
         } else {
-        ?>
+            echo '<div> Cant find link with hash: ' . $hash . ' </div>';
+        }
+    } else {
+?>
 
-        <form action="generate.php" method="post" >
-            Link: <input type="text" name="link" autofocus="autofocus" />
+    <h1> Create short link: </h1>
+    <form action="generate.php" method="post" >
+        <fieldset>
+            <label for="link"> Link </label>
+            <input type="text" id="link" name="link" 
+                autofocus="autofocus" style="width:200px; height:30px;" />
             <input type="submit" value="Create" />
-        </form>
-        <?php } ?>
+        </fieldset>
+    </form>
+
+<?php } ?>
+    
 </body>
 </html>
 
