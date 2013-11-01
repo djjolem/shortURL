460474d30012be1d86636df7e913c81c3aee169f
djjolem <djjolem@gmail.com>
2013-11-01 12:00:57 +0100

    Update index.php
    Login and create account.

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 </head>
 
 <body>
-<div class="container">
+<div class="container" id="container">
 <?php
     // redirect to another site
     include 'DBConnection.php';
@@ -52,7 +52,7 @@
             <input class="form-control" type="text" id="link" name="link" autofocus="autofocus"
                 placeholder="Enter link" />
             <span class="input-group-btn"> 
-                <button class="btn btn-primary" type=button" onclick="submit();"> Create </button>
+                <button class="btn btn-primary" type="button" onclick="submit();"> Create </button>
             </span>
         </div>
         </div>
@@ -83,10 +83,41 @@
 
 <?php 
         }
-    } 
+    }
 ?>
 
-</div>
+	<!-- login form -->
+	<form class="form-horizontal" role="form">
+		<div class="form-group">
+    		<label for="inputEmail3" class="col-sm-2 control-label">Email</label>
+    		<div class="col-sm-10">
+      			<input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+    		</div>
+  		</div>
+  		<div class="form-group">
+    		<label for="inputPassword3" class="col-sm-2 control-label">Password</label>
+    		<div class="col-sm-10">
+      			<input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+    		</div>
+  		</div>
+  		<div class="form-group">
+    		<div class="col-sm-offset-2 col-sm-10">
+      			<div class="checkbox">
+        			<label><input type="checkbox"> Remember me </label>
+      			</div>
+    		</div>
+  		</div>
+  		<div class="form-group">
+    		<div class="col-sm-offset-2 col-sm-10">
+      			<button type="submit" class="btn btn-default">Sign in</button>
+    		</div>
+  		</div>
+	</form>
+
+	<!-- add create account -->
+	<div> Create account </div>
+
+   
+</div id=container> 
 </body>
 </html>
-
